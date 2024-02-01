3323f540a50c9789b32d5f5df93f395d36acde33
kajuran7 <kajubass@gmail.com>
2024-02-01 11:33:59 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,29 @@
+
+let allMusic = [
+    {
+      name: "Hukum",
+      artist: "AniRudh",
+      img: "1",
+      src: "1"
+    },
+    {
+      name: "Baddass",
+      artist: "Anil🐿️Rudh",
+      img: "2",
+      src: "2"
+    },
+    {
+      name: "Master The Blaster",
+      artist: "Anil🐿️Rudh",
+      img: "3",
+      src: "3"
+    },
+    {
+      name: "Vikram",
+      artist: "AniRudh",
+      img: "4",
+      src: "4"
+    },
+
+   
+  ];
